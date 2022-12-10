b7204e136021ca7af0bdbe10b796a353ac3b5200
Thong Nguyen <thongnguyen@pixelcent.com>
2022-12-10 19:49:34 +0700

    Build all UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { useState, useMemo, useEffect } from "react";
@@ -8,8 +8,11 @@ function App() {
 
   const schema = useMemo(() => {
     return yup.object().shape({
-      example: yup.number().required("This fied is required").typeError("This field must be number"),
-      example2: yup.string()
+      name: yup.string().required("Please fill in employee's name"),
+      age: yup.number().typeError("Please fill in a number").required("Please fill in employee's age"),
+      country: yup.string().required("Please fill in employee's country"),
+      position: yup.string().required("Please fill in employee's position"),
+      wage: yup.number().typeError("Please fill in a number").required("Please fill in employee's wage"),
     })
   });
 
@@ -17,6 +20,7 @@ function App() {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema),
@@ -29,16 +33,54 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="AddEmployee">
+        <form onSubmit={handleSubmit(onSubmit)}>
 
-        <input {...register("example")} />
-        {errors?.example && errors?.example?.message}
+          <label>Name:</label>
+          <input {...register("name")} />
+          <p>{errors?.name && errors?.name?.message}</p>
 
-        <input {...register("example2")} />
-        {errors?.example2 && errors?.example?.message}
+          <label>Age:</label>
+          <input {...register("age")} />
+          <p>{errors?.age && errors?.age?.message}</p>
 
-        <button type='submit'>Submit !</button>
-      </form>
+          <label>Country:</label>
+          <input {...register("country")} />
+          <p>{errors?.country && errors?.country?.message}</p>
+
+          <label>Position:</label>
+          <input {...register("position")} />
+          <p>{errors?.position && errors?.position?.message}</p>
+
+          <label>Wage (year):</label>
+          <input {...register("wage")} />
+          <p>{errors?.wage && errors?.wage?.message}</p>
+
+          <button type='submit'>Add Employee</button>
+
+          <button type='button'>View Employee</button>
+        </form>
+      </div>
+
+      <div className="ViewEditEmployees">
+        <h1>All Employees: </h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Country</th>
+            <th>Position</th>
+            <th>Wage</th>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Country</td>
+            <td>Position</td>
+            <td>Wage</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
